08eca76584ee65c1c75d9663177ee6ccdaa47843
Jeffrey Johnston <JJohnston1822@gmail.com>
2021-05-29 23:01:13 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -14,13 +14,32 @@ const INITIAL_EXPENSES = [
     id: "e3",
     title: "Car Insurance",
     amount: 294.67,
-    date: new Date(2021, 2, 28),
+    date: new Date(2021, 1, 28),
   },
   {
     id: "e4",
     title: "New Desk (Wooden)",
     amount: 450,
-    date: new Date(2021, 5, 12),
+    date: new Date(2021, 3, 12),
+  },
+  {
+    id: "e5",
+    title: "Car Repair",
+    amount: 495.78,
+    date: new Date(2020, 6, 4),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2020, 7, 12) },
+  {
+    id: "e6",
+    title: "Vacation",
+    amount: 2000,
+    date: new Date(2020, 11, 28),
+  },
+  {
+    id: "e7",
+    title: "Dentist",
+    amount: 200,
+    date: new Date(2021, 0, 11),
   },
 ];
 
